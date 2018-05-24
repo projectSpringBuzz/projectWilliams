f0a500acf259dd3af03ce5a8b7a52e53f4adf83e
William Marchan Aranda <william.marchan@bbva.com>
2018-05-24 12:52:35 -0500

    update script sql

--- UserRRatings/src/main/resources/DDL.sql
+++ UserRRatings/src/main/resources/DDL.sql
@@ -8,20 +8,30 @@ USE maddoxBD;
 
 DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS details;
+DROP TABLE IF EXISTS notes;
 
+/* this table is a mock */
 CREATE TABLE orders(
 	id INT NOT NULL AUTO_INCREMENT,
     customerName VARCHAR(50),
     phoneNumber VARCHAR(15),
     PRIMARY KEY ( id )
 );
+/**************************/
 
 CREATE TABLE details(
    id INT NOT NULL AUTO_INCREMENT,
    orderID INT NOT NULL,
    productName VARCHAR(120) NOT NULL,
    rating INT,
-   notes MEDIUMTEXT,
    PRIMARY KEY ( id ),
    FOREIGN KEY (orderID) REFERENCES orders(id)
+);
+
+CREATE TABLE notes(
+   id INT NOT NULL AUTO_INCREMENT,
+   orderID INT NOT NULL,
+   notes MEDIUMTEXT,
+   PRIMARY KEY ( id ),
+   FOREIGN KEY (orderID) REFERENCES details(orderID)
 );
