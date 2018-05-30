52f00c4d1753d6b015483496177101204989b59f
William Marchan Aranda <william.marchan@bbva.com>
2018-05-30 14:34:58 -0500

    kafka with producer and 2 consumer, it need 2 topic

--- UserRRatings/src/main/resources/DDL.sql
+++ UserRRatings/src/main/resources/DDL.sql
@@ -17,54 +17,38 @@ DROP PROCEDURE IF EXISTS SP_FIND_NOTES_BY_ORDERID;
 
 SET foreign_key_checks = 1;
 
-/* this table is a mock */
-CREATE TABLE orders(
-	id INT NOT NULL AUTO_INCREMENT,
-    customerName VARCHAR(50),
-    phoneNumber VARCHAR(15),
-    PRIMARY KEY ( id )
-);
-/**************************/
-
 CREATE TABLE details(
-   id INT NOT NULL AUTO_INCREMENT,
-   orderID INT NOT NULL,
-   productName VARCHAR(120) NOT NULL,
-   rating INT,
-   PRIMARY KEY ( id ),
-   FOREIGN KEY (orderID) REFERENCES orders(id)
+   	id INT(11) NOT NULL AUTO_INCREMENT,
+	orderID INT(11) NOT NULL,
+	product_name VARCHAR(120) NOT NULL,
+	rating INT(11) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE notes(
    id INT NOT NULL AUTO_INCREMENT,
    orderID INT NOT NULL,
    notes MEDIUMTEXT,
-   PRIMARY KEY ( id ),
-   FOREIGN KEY (orderID) REFERENCES orders(id)
+   PRIMARY KEY ( id )
 );
 
-INSERT INTO `orders` (`id`, `customerName`, `phoneNumber`) VALUES
-	(1, 'william 1', '12345678'),
-	(2, 'william 2', '12345676'),
-	(3, 'william 3', '12345678'),
-	(4, 'william 4', '12345679');
-
-INSERT INTO `details` (`id`, `orderID`, `productName`, `rating`) VALUES
-	(1, 1, 'product 1', 4),
-	(2, 1, 'product 2', 5),
-	(3, 1, 'product 3', 6),
+INSERT INTO `details` (`id`, `orderID`, `product_name`, `rating`) VALUES
+	(1, 1, 'product 1', 2),
+	(2, 1, 'product 2', 3),
+	(3, 1, 'product 3', 2),
 	(4, 2, 'product 1', 4),
 	(5, 2, 'product 4', 2);
 	
 INSERT INTO `notes` (`id`, `orderID`, `notes`) VALUES
-	(1, 1, 'text for table'),
-	(2, 1, 'text for table 2');
+	(1, 1, 'asdas'),
+	(2, 1, 'qweqwe'),
+	(3, 2, 'gdfd');
 	
 DELIMITER //
 CREATE PROCEDURE `SP_FIND_PRODUCTS_BY_ORDERID` (IN orderID INT)
 BEGIN
 	
-	SELECT d.id, d.productName, d.rating 
+	SELECT d.orderID, d.product_name, d.rating, d.id
 	FROM details d
 	WHERE d.orderID = orderID;
 
@@ -74,7 +58,7 @@ DELIMITER //
 CREATE PROCEDURE `SP_FIND_NOTES_BY_ORDERID` (IN orderID INT)
 BEGIN
 	
-	SELECT n.id, n.notes 
+	SELECT n.id, n.notes, n.orderID 
 	FROM notes n
 	WHERE n.orderID = orderID;
 
